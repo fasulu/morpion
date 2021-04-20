7929982793bca76e8c2d68f93ad30e4fc05b0b58
karim <karim.mezouar@gmail.com>
2021-04-20 17:46:43 +0200

    Modification de css et de App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Game from "./Components/Game"
 import "./App.css";
 
+
 class App extends React.Component {
 
 
@@ -9,8 +10,15 @@ class App extends React.Component {
 
   render() {
     return (
+     
+       
+      <div className = "bg-image"> 
+        <h1 className = "player">player one (X): </h1>
+        <h1 className = "player">player two (O): </h1>
+        <Game />
+      </div>
+     
 
-      <Game />
 
     );
   }
